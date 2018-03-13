e576652b38855baf31d83f709e84cfdddb6ae324
maaz98 <maaz98@mail.umkc.edu>
2018-03-13 16:15:27 -0500

    Update config.java

--- CheckDo/app/src/main/java/dev/majed/checkdo/config.java
+++ CheckDo/app/src/main/java/dev/majed/checkdo/config.java
@@ -1,7 +1,7 @@
-package dev.majed.checkdo;
+ackage dev.majed.checkdo;
 
 public class config {
 
-    public static final String EMAIL ="checkdoapp@gmail.com"; //your-gmail-username
-    public static final String PASSWORD ="checkdo123"; //your-gmail-password
+    public static final String EMAIL ="checkdo.tapp@gmail.com"; //your-gmail-username
+    public static final String PASSWORD ="checkdo651"; //your-gmail-password
 }
